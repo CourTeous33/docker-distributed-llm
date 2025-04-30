599571e562fc72051e15fdc7cc353be7f75fd961
Evan Chen <echen48@illinois.edu>
2025-04-30 13:19:55 -0500

    setup branch

--- backend/config.py
+++ backend/config.py
@@ -1,6 +1,6 @@
 # Depends on what model you downloaded using the `model-downloader`
-MODEL_PATH = "/models/llama3_2_1b_instruct_q40/dllama_model_llama3_2_1b_instruct_q40.m"
-TOKENIZER_PATH = "/models/llama3_2_1b_instruct_q40/dllama_tokenizer_llama3_2_1b_instruct_q40.t"
+MODEL_PATH = "/models/llama3_2_1b_instruct_f32/dllama_model_llama3.2-1b-instruct_f32.m"
+TOKENIZER_PATH = "/models/llama3_2_1b_instruct_f32/dllama_tokenizer_llama3.t"
 # Depends on the docker-compose structure and how many workers you want to support (note the backend itself is also a worker, so there should be 2^N - 1 workers)
 WORKER_URLS = [
     "http://worker1:5000",
@@ -11,7 +11,7 @@ WORKER_URLS = [
 WORKER_IPS = ["172.20.0.11:9998", "172.20.0.12:9998", "172.20.0.13:9998"]
 
 # Estimated range to simulate 
-LATENCY_MIN = 0.01 # ex: 0.01 = 10 milliseconds
-LATENCY_MAX = 0.05 # ex: 0.05 = 50 milliseconds
+LATENCY_MIN = 0.06 # ex: 0.01 = 10 milliseconds
+LATENCY_MAX = 0.12 # ex: 0.05 = 50 milliseconds
 
 N_THREADS = 3 # CPU threads to use for the root node (availability depends on docker compose and your machine)
